17cc1f6c7b48697286f9140bd36200972a9e3e7c
Tal <tal.lavon1@gmail.com>
2019-04-11 18:31:03 +0300

    syscall.c update

--- syscall.c
+++ syscall.c
@@ -133,13 +133,14 @@ syscall(void)
 {
   int num;
   struct proc *curproc = myproc();
+  struct kthread *curthread = mythread();
 
-  num = curproc->tf->eax;
+  num = curthread->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    curproc->tf->eax = syscalls[num]();
+    curthread->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
-    curproc->tf->eax = -1;
+    curthread->tf->eax = -1;
   }
 }
