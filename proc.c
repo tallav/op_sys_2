07a53f8bfc05ed88e33fd792c73a212f428087fc
Tal <tal.lavon1@gmail.com>
2019-04-11 17:42:22 +0300

    scheduler update

--- proc.c
+++ proc.c
@@ -383,6 +383,7 @@ void
 scheduler(void)
 {
   struct proc *p;
+  struct kthread *t;
   struct cpu *c = mycpu();
   c->proc = 0;
   
@@ -393,22 +394,33 @@ scheduler(void)
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
+      int threadReady = 0;
+      for (int i = 0; i < NTHREAD; i++){
+        t = p->threads[i];
+        if(t->state != RUNNABLE)
+          continue;
+        else
+          threadReady = 1;
+      }
+      if(!threadReady)
         continue;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
       c->proc = p;
+      c->thread = t;
       switchuvm(p);
       p->state = RUNNING;
+      t->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
+      swtch(&(c->scheduler), t->context);
       switchkvm();
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
+      c->thread = 0;
     }
     release(&ptable.lock);
 
