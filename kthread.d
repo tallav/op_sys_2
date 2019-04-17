ae8f3650d5ff620d910c5842b7b6f6c786893945
tallav <tal.lavon1@gmail.com>
2019-04-17 11:30:55 +0300

    kthread_create update, call function works but still panic

--- kthread.d
+++
@@ -1,2 +0,0 @@
-kthread.o: kthread.c /usr/include/stdc-predef.h types.h defs.h param.h \
- memlayout.h mmu.h x86.h proc.h spinlock.h kthread.h
--- myprog.d
+++
@@ -1,2 +0,0 @@
-myprog.o: myprog.c /usr/include/stdc-predef.h param.h types.h stat.h \
- user.h fs.h fcntl.h syscall.h traps.h memlayout.h
