d5eaeecf19c1b605ea6f3748587833b9fe7457c4
tallav <tal.lavon1@gmail.com>
2019-05-01 20:43:10 +0300

    tal version with tree tests in sanity

--- sanity.d
+++
@@ -1,3 +0,0 @@
-sanity.o: sanity.c /usr/include/stdc-predef.h param.h types.h stat.h \
- user.h fs.h fcntl.h syscall.h traps.h memlayout.h tournament_tree.h \
- kthread.h
--- tree.d
+++
@@ -1,2 +0,0 @@
-tree.o: tree.c /usr/include/stdc-predef.h types.h user.h \
- tournament_tree.h kthread.h
